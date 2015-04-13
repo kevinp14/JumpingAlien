bc2a1188a01fde13ce249454f9dbeaa93b4d0c62
JasperMarien <boy-jasper@hotmail.com>
2015-04-13 14:58:27 +0200

    Update School.java
    kleine toevoegingen

--- OGP1415-JumpingAlien/src/jumpingalien/model/School.java
+++ OGP1415-JumpingAlien/src/jumpingalien/model/School.java
@@ -1,5 +1,19 @@
 package jumpingalien.model;
 
-public class School {
+import java.util.Collection;
 
+public class School {
+	private Collection<Slime> slimes;
+	
+	public School(){
+		this.slimes = null;
+	}
+	
+	public void addSlimeToSchool(Slime slime){
+		this.slimes.add(slime);
+	}
+	
+	public void removeSlimeFromSchool(Slime slime){
+		this.slimes.remove(slime);
+	}
 }
