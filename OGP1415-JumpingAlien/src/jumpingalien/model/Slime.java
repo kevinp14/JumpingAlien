e20cce8b072ca4bba2fb5275fb34ced441a413c8
JasperMarien <boy-jasper@hotmail.com>
2015-04-13 15:04:11 +0200

    Update Slime.java

--- OGP1415-JumpingAlien/src/jumpingalien/model/Slime.java
+++ OGP1415-JumpingAlien/src/jumpingalien/model/Slime.java
@@ -4,38 +4,62 @@ import be.kuleuven.cs.som.annotate.Basic;
 import jumpingalien.util.Sprite;
 
 public class Slime extends Mazub{
-	private double positionX;
-	private double positionY;
-	private Sprite[] spriteList;
+	//private double positionX;
+	//private double positionY;
+	//private Sprite[] spriteList;
 	private School school;
-	private int timeMoving;
-	private Direction lastDirection;
+	//private int timeMoving;
+	//private Direction lastDirection;
 
-	public Slime (int positionX, int positionY, Sprite[] spriteList) {
-		this.positionX = (double)positionX/100d;
-		this.positionY = (double)positionY/100d;
-		this.spriteList = spriteList;
+	public Slime (int positionX, int positionY, Sprite[] spriteList, School school) {
+		//this.positionX = (double)positionX/100d;
+		//this.positionY = (double)positionY/100d;
+		//this.spriteList = spriteList;
+		super(positionX, positionY, spriteList);
 		this.school = school;
-		this.timeMoving = 0;
-		this.lastDirection = Direction.STALLED;
+		//this.timeMoving = 0;
+		//this.lastDirection = Direction.STALLED;
 	}
 	
-	@Basic
-	public double[] getPosition() {
-		double[] position = new double[]{this.positionX, this.positionY};
-		return position;
-	}
+	//public void advanceTime(double dt){
+	//	
+	//}
+	
+	//@Basic
+	//public double[] getPosition() {
+	//	double[] position = new double[]{this.positionX, this.positionY};
+	//	return position;
+	//}
 	
 	@Basic
 	public School getSchool(){
 		return this.school;
 	}
 	
-	public void setSchool(School school){
-		this.school = school;
+	public void setSchool(School newSchool){
+		this.school.removeSlimeFromSchool(this);
+		this.school = newSchool;
+		newSchool.addSlimeToSchool(this);
 	}
 	
-	public void advanceTime(double dt){
+	//public Sprite getCurrentSprite(){
+	//	if (this.timeMoving == 0){
+	//		return this.spriteList[0];
+	//	}
+	//	else{
+	//		return this.spriteList[0];
+	//	}
+	//}
+	
+	//public Direction getDirection(){
+	//	return this.lastDirection;
+	//}
+	
+	public void startDuck(){
+		
+	}
+	
+	public void startJump(){
 		
 	}
 	
