be8a744f9c881a38c967d01956686b42180ded02
JasperMarien <boy-jasper@hotmail.com>
2015-04-13 15:00:05 +0200

    Update Shark.java

--- OGP1415-JumpingAlien/src/jumpingalien/model/Shark.java
+++ OGP1415-JumpingAlien/src/jumpingalien/model/Shark.java
@@ -1,30 +1,35 @@
 package jumpingalien.model;
 
-import be.kuleuven.cs.som.annotate.Basic;
+//import be.kuleuven.cs.som.annotate.Basic;
 import jumpingalien.util.Sprite;
 
-public class Shark{
-	private double positionX;
-	private double positionY;
-	private Sprite[] spriteList;
+public class Shark extends Mazub{
+	//private double positionX;
+	//private double positionY;
+	//private Sprite[] spriteList;
 	
 	public Shark(int positionX, int positionY, Sprite[] spriteList) {
-		this.positionX = (double)positionX/100d;
-		this.positionY = (double)positionY/100d;
-		this.spriteList = spriteList;
+		//this.positionX = (double)positionX/100d;
+		//this.positionY = (double)positionY/100d;
+		//this.spriteList = spriteList;
+		super(positionX, positionY, spriteList);
 	}
 
-	public Sprite getCurrentSprite(){
+	//public void advanceTime(double dt){
+		
+	//}
+
+	//public Sprite getCurrentSprite(){
+	//	return this.getCurrentsprite();
+	//}
 	
-	}
+	//@Basic
+	//public double[] getPosition() {
+	//	double[] position = new double[]{this.getPosition()[0], this.getPosition()[1]};
+	//	return position;
+	//}
 	
-	@Basic
-	public double[] getPosition() {
-		double[] position = new double[]{this.positionX, this.positionY};
-		return position;
-	}
-	
-	public void advanceTime(double dt){
+	public void startDuck(){
 		
 	}
 }
