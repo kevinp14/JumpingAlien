68bc60e2f8dcdc0947d20a014cf5b5e2675bb92b
JasperMarien <boy-jasper@hotmail.com>
2015-04-13 14:55:28 +0200

    Update Plant.java
    denk dat het zo beter is?

--- OGP1415-JumpingAlien/src/jumpingalien/model/Plant.java
+++ OGP1415-JumpingAlien/src/jumpingalien/model/Plant.java
@@ -1,23 +1,17 @@
 package jumpingalien.model;
 
-import be.kuleuven.cs.som.annotate.Basic;
 import jumpingalien.util.Sprite;
 
-public class Plant {
-	private double positionX;
-	private double positionY;
-	private Sprite[] spriteList;
+public class Plant extends Mazub{
+	//private double positionX;
+	//private double positionY;
+	//private Sprite[] spriteList;
 	
 	public Plant(int positionX, int positionY, Sprite[] spriteList){
-		this.positionX = (double)positionX/100d;
-		this.positionY = (double)positionY/100d;
-		this.spriteList = spriteList;
-	}
-	
-	@Basic
-	public double[] getPosition() {
-		double[] position = new double[]{this.positionX, this.positionY};
-		return position;
+		//this.positionX = (double)positionX/100d;
+		//this.positionY = (double)positionY/100d;
+		//this.spriteList = spriteList;
+		super(positionX, positionY, spriteList);
 	}
 	
 	public void advanceTime(double dt){
