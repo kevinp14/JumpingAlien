a2b7bb894367de629be966d63aee2775d65b9d8f
JasperMarien <boy-jasper@hotmail.com>
2015-05-22 15:57:46 +0200

    Update Assignment.java
    Type

--- OGP1415-JumpingAlien/src/jumpingalien/model/statement/Assignment.java
+++ OGP1415-JumpingAlien/src/jumpingalien/model/statement/Assignment.java
@@ -8,20 +8,22 @@ import jumpingalien.part3.programs.SourceLocation;
 public class Assignment implements Statement {
 	
 	private String variableName;
+	private Type<?> variableType;
 	private Expression value;
 	private SourceLocation sourceLocation;
 	
 	public Assignment(String variableName, Type<?> variableType,
 	Expression value, SourceLocation sourceLocation){
 		this.variableName = variableName;
+		this.variableType = variableType;
 		this.value = value;
 		this.sourceLocation = sourceLocation;
 	}
 
 	@Override
 	public void execute(Program program, Expression condition) {
-		Type<?> type = (Type<?>)this.value.evaluate(program);
-		program.setObjectByName(this.variableName, type);
+		Type<?> val = (Type<?>)this.value.evaluate(program);
+		program.setObjectByName(this.variableName, this.variableType, val);
 	}
 
 	@Override
