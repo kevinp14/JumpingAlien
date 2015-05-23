5e7656d94a8b2203c51b07c0213cbf0b7e6ae5de
JasperMarien <boy-jasper@hotmail.com>
2015-05-23 22:02:02 +0200

    Update StartJump.java

--- OGP1415-JumpingAlien/src/jumpingalien/model/statement/StartJump.java
+++ OGP1415-JumpingAlien/src/jumpingalien/model/statement/StartJump.java
@@ -9,7 +9,7 @@ import jumpingalien.part3.programs.SourceLocation;
  * A class to make a game object jump.
  * 
  * @author	Kevin Peeters (Tweede fase ingenieurswetenschappen)
- * 			Jasper Mariën (Tweede fase ingenieurswetenschappen)
+ * 			Jasper MariÃ«n (Tweede fase ingenieurswetenschappen)
  * @version 1.0
  */
 public class StartJump implements Statement {
@@ -49,6 +49,13 @@ public class StartJump implements Statement {
 	@Override
 	public SourceLocation getSourceLocation() {
 		return this.sourceLocation;
+	}
+	
+	@Override
+	public void executeForGivenObject(Program program, Expression condition,
+			Object object) {
+		GameObject gameObject = (GameObject) object;
+		gameObject.startJump();
 	}
 
 }
