0caf181c9234c358c588611f010933367af795d7
JasperMarien <boy-jasper@hotmail.com>
2015-05-23 21:56:39 +0200

    Update Assignment.java

--- OGP1415-JumpingAlien/src/jumpingalien/model/statement/Assignment.java
+++ OGP1415-JumpingAlien/src/jumpingalien/model/statement/Assignment.java
@@ -9,7 +9,7 @@ import jumpingalien.part3.programs.SourceLocation;
  * A class to assign a value to a variable.
  * 
  * @author	Kevin Peeters (Tweede fase ingenieurswetenschappen)
- * 			Jasper Mariën (Tweede fase ingenieurswetenschappen)
+ * 			Jasper MariÃ«n (Tweede fase ingenieurswetenschappen)
  * @version 1.0
  */
 public class Assignment implements Statement {
@@ -62,6 +62,13 @@ public class Assignment implements Statement {
 	@Override
 	public SourceLocation getSourceLocation() {
 		return this.sourceLocation;
+	}
+	
+	@Override
+	public void executeForGivenObject(Program program, Expression condition,
+			Object object) {
+		Object val = this.value.evaluate(program);
+		program.setObjectByName(this.variableName, this.variableType, val);
 	}
 
 }
