cdd752e3148c12a4b71b55b13444a34c79f58099
JasperMarien <boy-jasper@hotmail.com>
2015-05-23 22:06:13 +0200

    Update WaitS.java

--- OGP1415-JumpingAlien/src/jumpingalien/model/statement/WaitS.java
+++ OGP1415-JumpingAlien/src/jumpingalien/model/statement/WaitS.java
@@ -8,7 +8,7 @@ import jumpingalien.part3.programs.SourceLocation;
  * A class to wait for a given amount of seconds.
  * 
  * @author	Kevin Peeters (Tweede fase ingenieurswetenschappen)
- * 			Jasper Mariën (Tweede fase ingenieurswetenschappen)
+ * 			Jasper MariÃ«n (Tweede fase ingenieurswetenschappen)
  * @version 1.0
  */
 public class WaitS implements Statement {
@@ -55,6 +55,18 @@ public class WaitS implements Statement {
 	@Override
 	public SourceLocation getSourceLocation() {
 		return this.sourceLocation;
+	}
+	
+	@Override
+	public void executeForGivenObject(Program program, Expression condition,
+			Object object) {
+		if (this.duration.evaluate(program) != null){
+			double dur = (double) this.duration.evaluate(program);
+			long time = (long) dur;
+			try {
+				Thread.sleep(time);
+			} catch (InterruptedException e) {}
+		}
 	}
 
 }
