3dd4ffae26fb914cd752cbd75fb66d814c6eaa74
JasperMarien <boy-jasper@hotmail.com>
2015-05-23 22:02:35 +0200

    Update StartRun.java

--- OGP1415-JumpingAlien/src/jumpingalien/model/statement/StartRun.java
+++ OGP1415-JumpingAlien/src/jumpingalien/model/statement/StartRun.java
@@ -10,7 +10,7 @@ import jumpingalien.part3.programs.SourceLocation;
  * A class to make a game object run.
  * 
  * @author	Kevin Peeters (Tweede fase ingenieurswetenschappen)
- * 			Jasper Mariën (Tweede fase ingenieurswetenschappen)
+ * 			Jasper MariÃ«n (Tweede fase ingenieurswetenschappen)
  * @version 1.0
  */
 public class StartRun implements Statement {
@@ -57,6 +57,13 @@ public class StartRun implements Statement {
 	@Override
 	public SourceLocation getSourceLocation() {
 		return this.sourceLocation;
+	}
+	
+	@Override
+	public void executeForGivenObject(Program program, Expression condition,
+			Object object) {
+		GameObject gameObject = (GameObject) object;
+		gameObject.startRun();
 	}
 
 }
