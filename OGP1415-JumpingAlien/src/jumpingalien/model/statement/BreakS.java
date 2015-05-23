6702c5c0b311f03fdd37d866a83573de1115fab9
JasperMarien <boy-jasper@hotmail.com>
2015-05-23 21:57:15 +0200

    Update BreakS.java

--- OGP1415-JumpingAlien/src/jumpingalien/model/statement/BreakS.java
+++ OGP1415-JumpingAlien/src/jumpingalien/model/statement/BreakS.java
@@ -9,7 +9,7 @@ import jumpingalien.part3.programs.SourceLocation;
  * A class to immediately terminate a while- or for-loop.
  * 
  * @author	Kevin Peeters (Tweede fase ingenieurswetenschappen)
- * 			Jasper Mariën (Tweede fase ingenieurswetenschappen)
+ * 			Jasper MariÃ«n (Tweede fase ingenieurswetenschappen)
  * @version 1.0
  */
 public class BreakS implements Statement {
@@ -50,6 +50,17 @@ public class BreakS implements Statement {
 	@Override
 	public SourceLocation getSourceLocation() {
 		return this.sourceLocation;
+	}
+	
+	@Override
+	public void executeForGivenObject(Program program, Expression condition,
+			Object object) {
+		SourceLocation srcLoc = condition.getSourceLocation();
+		boolean cond = (boolean)condition.evaluate(program);
+		while (cond) {
+			break;
+		}
+		this.sourceLocation = srcLoc;
 	}
 
 }
