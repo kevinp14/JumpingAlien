2dbcb79ec9b867493ae0c5a11126d29067e8a9e1
JasperMarien <boy-jasper@hotmail.com>
2015-05-23 23:00:01 +0200

    Update LessThanOrEqual.java

--- OGP1415-JumpingAlien/src/jumpingalien/model/expression/LessThanOrEqual.java
+++ OGP1415-JumpingAlien/src/jumpingalien/model/expression/LessThanOrEqual.java
@@ -7,7 +7,7 @@ import jumpingalien.part3.programs.SourceLocation;
  * A class to check if a double is less than or equal to the other.
  * 
  * @author	Kevin Peeters (Tweede fase ingenieurswetenschappen)
- * 			Jasper Mariën (Tweede fase ingenieurswetenschappen)
+ * 			Jasper MariÃ«n (Tweede fase ingenieurswetenschappen)
  * @version 1.0
  */
 public class LessThanOrEqual extends DoubleExpression implements Expression {
@@ -35,7 +35,7 @@ public class LessThanOrEqual extends DoubleExpression implements Expression {
 	 */
 	@Override
 	public Object evaluate(Program program) {
-		return this.lessThanOrEqual();
+		return this.lessThanOrEqual(program);
 	}
 	
 	/**
